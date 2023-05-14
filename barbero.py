3a10c62ca25e0254d2a5184e6aadd0514638188c
crltsnch <crltsnch@myuax.com>
2023-05-14 22:25:01 +0200

    clase barbero acabada

--- barbero.py
+++ barbero.py
@@ -9,4 +9,9 @@ class Barbero:
             print("El barbero está durmiendo")
             async with barberia.fila:
                 print("El barbero se ha despertado")
-                
+                self.durmiendo = False
+                async with barberia.silla:
+                    print("El barbero está afeitando a un cliente")
+                    await asyncio.sleep(2)
+                    print("El barbero terminó de afeitar a un cliente")
+                self.durmiendo = True
