1ba3b6bdf65b165f92e141b4dabea20d8c901698
crltsnch <crltsnch@myuax.com>
2023-05-15 08:40:59 +0200

    clase cliente comentada

--- cliente.py
+++ cliente.py
@@ -1,14 +1,14 @@
 from barberia import *
 
 class Cliente:
-    def __init__(self, id):
+    def __init__(self, id):  #atributo identificador del cliente
         self.id = id
     
     async def afeitar(self, barberia):
-        async with barberia.fila:
+        async with barberia.fila:   #adquirir permiso del semaforo  par aentrar a la barbería
             print(f"El cliente {self.id} se sienta en la fila de espera")
-            async with barberia.silla:
+            async with barberia.silla:   #asquierir permiso del lock para sentarse en la silla
                 print(f"El cliente {self.id} está siendo atendido por el barbero")
-                await asyncio.sleep(2)
+                await asyncio.sleep(2)  #simular el tiempo que tarda el barbero en afeitar al cliente
                 print(f"El cliente {self.id} terminó de ser afeitado y se va de la barberia")
                 
