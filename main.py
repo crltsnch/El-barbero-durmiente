5da31903758b60db8bd4c7ee016b67021114eaa7
crltsnch <crltsnch@myuax.com>
2023-05-15 13:59:45 +0200

    main acabado

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from cliente import *
 from barbero import *
 
 async def main():
-    barberia = Barberia()
-    barbero = Barbero()
-    clientes = [Cliente(i) for i in range(10)]
-    tareas_clientes = [cliente.afeitar(barberia) for cliente in clientes]
-    tarea_barbero = barbero.trabajar(barberia)
-    await asyncio.gather(tarea_barbero, *tareas_clientes)
+    barberia = Barberia()  #creamos intancia barberia
+    barbero = Barbero()  #creamos instancia barbero
+    clientes = [Cliente(i) for i in range(10)]  #creamos una lista de 10 clientes
+    tareas_clientes = [cliente.afeitar(barberia) for cliente in clientes]  #creamos una lista de tareas de los clientes llamando al método afeitar
+    tarea_barbero = barbero.trabajar(barberia)  #llamar al método trabajar del barero y ejecutarlo en un bucle infinito
+    await asyncio.gather(tarea_barbero, *tareas_clientes) #esperamos a que todas las tareas (afeitado de cliente sy trabajo del barbero) se completen
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main())  #ejecutamos con asyncio
