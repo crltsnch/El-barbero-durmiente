e9eca378ce93158c5ddd97c393ed492330814d7b
crltsnch <crltsnch@myuax.com>
2023-05-15 08:38:23 +0200

    barberia comentada

--- barberia.py
+++ barberia.py
@@ -1,8 +1,9 @@
 import asyncio
 
 class Barberia:
-    def __init__(self):
-        self.silla = asyncio.Lock()
-        self.fila = asyncio.Semaphore(4)
+    def __init__(self):   #dos atributos: silla y fila
+        self.silla = asyncio.Lock()    #lock para la silla
+        self.fila = asyncio.Semaphore(4)  #semaforo para controlar la acantidad de clientes en la fila
+        
     
     
