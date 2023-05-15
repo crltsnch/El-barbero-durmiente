a37b57463d04fd1c2a23a52881aacb069e8ff796
crltsnch <crltsnch@myuax.com>
2023-05-15 13:56:50 +0200

    clase barbero acabada

--- barbero.py
+++ barbero.py
@@ -7,11 +7,11 @@ class Barbero:
     async def trabajar(self, barberia):
         while True:
             print("El barbero está durmiendo")
-            async with barberia.fila:
+            async with barberia.fila:   #permiso del semaforo para entrar a la barbería
                 print("El barbero se ha despertado")
                 self.durmiendo = False
-                async with barberia.silla:
+                async with barberia.silla:  #permiso del lock para sentarse en la silla del barbero
                     print("El barbero está afeitando a un cliente")
-                    await asyncio.sleep(2)
+                    await asyncio.sleep(2)  #simular el tiempo de afeitado
                     print("El barbero terminó de afeitar a un cliente")
                 self.durmiendo = True
